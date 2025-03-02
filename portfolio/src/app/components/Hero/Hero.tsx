e5cd248766daa2523e3300149dd590687c298c58
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-03-02 07:56:02 +0000

    Hero Page Layout finshed

--- portfolio/src/app/components/Hero/Hero.tsx
+++ portfolio/src/app/components/Hero/Hero.tsx
@@ -1,11 +1,19 @@
+import { motion } from "framer-motion";
+
 import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
     <>
       <section className={styles.container}>
-        <p className={styles.title}>Hi I'm Caitlin</p>
-        <p>Web Dev, animator, cool cat</p>
+        <h1 className={styles.title}>
+          hi i'm <span className={styles.caitlin}>Caitlin</span>
+        </h1>
+        <p className={styles.description}>
+          FRONT END WEB DEVELOPER <br />
+          BASED IN THE UK <br />
+          <span className={styles.portText}>PORTFOLIO © 202</span>5
+        </p>
       </section>
     </>
   );
